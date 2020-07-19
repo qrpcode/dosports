bc278e72107be1fb5674833d5785e9c7b76d3a57
Xrafece <286402807@qq.com>
2020-07-20 04:42:50 +0800

    add Constant.java,modify WeChat constant and Mysql database constant

--- 后端代码/src/main/java/com/xrafece/do_sport/controller/WxLoginController.java
+++ 后端代码/src/main/java/com/xrafece/do_sport/controller/WxLoginController.java
@@ -1,6 +1,7 @@
 package com.xrafece.do_sport.controller;
 
 import com.xrafece.do_sport.dto.WxSession;
+import com.xrafece.do_sport.util.Constant;
 import com.xrafece.do_sport.util.HttpClientUtil;
 import com.xrafece.do_sport.util.JsonUtil;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.HashMap;
 import java.util.Map;
 
+
 /**
  * @author Xrafece
  */
@@ -16,16 +18,15 @@ import java.util.Map;
 @RequestMapping("wx")
 public class WxLoginController {
     @RequestMapping("login")
-    public WxSession wxLogin(String code, Integer gender, String nickName, String avatar){
-        String url = "https://api.weixin.qq.com/sns/jscode2session";
-        System.out.println(gender+" -"+nickName+"-"+avatar);
+    public WxSession wxLogin(String code, Integer gender, String nickName, String avatar) {
+        System.out.println(gender + " -" + nickName + "-" + avatar);
         Map<String, String> param = new HashMap<>();
-        param.put("appid", "wx5a239421be7bc332");
-        param.put("secret", "d08713115d270cf452d9291671fa8c3e");
+        param.put("appid", Constant.WX_APPID);
+        param.put("secret", Constant.WX_SECRET);
         param.put("js_code", code);
-        param.put("grant_type", "authorization_code");
+        param.put("grant_type", Constant.WX_GRANT_TYPE);
 
-        String wxResult = HttpClientUtil.doGet(url, param);
+        String wxResult = HttpClientUtil.doGet(Constant.WX_REQUEST_URL, param);
         System.out.println(wxResult);
 
         WxSession wxSession = JsonUtil.jsonToPojo(wxResult, WxSession.class);
---
+++ 后端代码/src/main/java/com/xrafece/do_sport/util/Constant.java
@@ -0,0 +1,12 @@
+package com.xrafece.do_sport.util;
+
+/**
+ * @author Xrafece
+ */
+public class Constant {
+    public static final String WX_REQUEST_URL = "https://api.weixin.qq.com/sns/jscode2session";
+    public static final String WX_APPID = "wx7a239421be7bc335";
+    public static final String WX_SECRET = "d08200154d270cf452d9291671fa8c3e";
+    public static final String WX_GRANT_TYPE = "authorization_code";
+
+}
